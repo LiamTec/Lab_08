e113d0941a3750e8e7e821ed347de9280889648f
LiamTec <liam.gonzales@tecsup.edu.pe>
2025-05-11 01:13:48 -0500

    quiz_analytics

--- quiz_api/quizzes/urls.py
+++ quiz_api/quizzes/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import QuizViewSet, QuestionViewSet, ChoiceViewSet
 from categories.views import CategoryViewSet, TagViewSet
 from user.views import QuizAttemptViewSet, ProfileViewSet, UserViewSet
+from quiz_analysis.views import QuestionStatViewSet,QuizActivityViewSet
 
 # Definición de los routers para las vistas basadas en ViewSets
 router = DefaultRouter()
@@ -14,6 +15,8 @@ router.register(r'tag', TagViewSet)
 router.register(r'quizattempt', QuizAttemptViewSet)
 router.register(r'profile', ProfileViewSet)
 router.register(r'users', UserViewSet)
+router.register(r'question-stats', QuestionStatViewSet, basename='question-stat')
+router.register(r'quiz-activities', QuizActivityViewSet, basename='quiz-activity')
 
 
 # Definir las rutas de la API
